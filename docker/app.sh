2f7672764c464bb6565dccd648442d297b166503
stuffch1k <elizaveta.sineva@urfu.me>
2023-06-17 15:17:46 +0500

    commit

--- docker/app.sh
+++ docker/app.sh
@@ -2,6 +2,6 @@
 
 alembic upgrade head
 
-cd src
+# cd src
 
-gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+# gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
